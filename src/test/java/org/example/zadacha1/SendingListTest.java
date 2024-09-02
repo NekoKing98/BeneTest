e0923916d0ab64442283f5710d125d777d43ac39
NekoKing <Smail-nik@mail.ru>
2024-09-02 19:39:38 +0800

    --_--

--- src/test/java/org/example/zadacha1/SendingListTest.java
+++ src/test/java/org/example/zadacha1/SendingListTest.java
@@ -19,56 +19,56 @@ public class SendingListTest {
 
     @Test
     public void testNearDate1(){
-        LocalDateTime test1 = LocalDateTime.of(2024, 3, 6, 18, 0);
-        LocalDateTime ans1 = LocalDateTime.of(2024, 3, 11, 18, 0);
-        assertEquals(ans1, sendingList.nearDate(test1));
+        LocalDateTime test = LocalDateTime.of(2024, 3, 6, 18, 0);
+        LocalDateTime ans = LocalDateTime.of(2024, 3, 11, 18, 0);
+        assertEquals(ans, sendingList.nearDate(test));
     }
     @Test
     public void testNearDate2(){
-        LocalDateTime test2 = LocalDateTime.of(2024, 3, 10, 18, 0);
-        LocalDateTime ans2 = LocalDateTime.of(2024, 3, 11, 18, 0);
-        assertEquals(ans2, sendingList.nearDate(test2));
+        LocalDateTime test = LocalDateTime.of(2024, 3, 10, 18, 0);
+        LocalDateTime ans = LocalDateTime.of(2024, 3, 11, 18, 0);
+        assertEquals(ans, sendingList.nearDate(test));
     }
 
     @Test
     public void testNearDate3(){
-        LocalDateTime test3 = LocalDateTime.of(2024, 3, 11, 18, 1);
-        LocalDateTime ans3 = LocalDateTime.of(2024, 3, 20, 18, 0);
-        assertEquals(ans3, sendingList.nearDate(test3));
+        LocalDateTime test = LocalDateTime.of(2024, 3, 11, 18, 1);
+        LocalDateTime ans = LocalDateTime.of(2024, 3, 20, 18, 0);
+        assertEquals(ans, sendingList.nearDate(test));
     }
 
     @Test
     public void testNearDate4(){
-        LocalDateTime test4 = LocalDateTime.of(2024, 5, 7, 18, 0);
-        LocalDateTime ans4 = LocalDateTime.of(2024, 5, 8, 18, 0);
-        assertEquals(ans4, sendingList.nearDate(test4));
+        LocalDateTime test = LocalDateTime.of(2024, 5, 7, 18, 0);
+        LocalDateTime ans = LocalDateTime.of(2024, 5, 8, 18, 0);
+        assertEquals(ans, sendingList.nearDate(test));
     }
 
     @Test
     public void testNearDate5(){
-        LocalDateTime test5 = LocalDateTime.of(2024, 4, 10, 17, 0);
-        LocalDateTime ans5 = LocalDateTime.of(2024, 4, 10, 18, 0);
-        assertEquals(ans5, sendingList.nearDate(test5));
+        LocalDateTime test = LocalDateTime.of(2024, 4, 10, 17, 0);
+        LocalDateTime ans = LocalDateTime.of(2024, 4, 10, 18, 0);
+        assertEquals(ans, sendingList.nearDate(test));
     }
 
     @Test
     public void testNearDate6(){
-        LocalDateTime test6 = LocalDateTime.of(2023, 12, 21, 18, 0);
-        LocalDateTime ans6 = LocalDateTime.of(2023, 12, 29, 18, 0);
-        assertEquals(ans6, sendingList.nearDate(test6));
+        LocalDateTime test = LocalDateTime.of(2023, 12, 21, 18, 0);
+        LocalDateTime ans = LocalDateTime.of(2023, 12, 29, 18, 0);
+        assertEquals(ans, sendingList.nearDate(test));
     }
 
     @Test
     public void testNearDate7(){
-        LocalDateTime test7 = LocalDateTime.of(2024, 5, 22, 18, 0);
-        LocalDateTime ans7 = LocalDateTime.of(2024, 6, 3, 18, 0);
-        assertEquals(ans7, sendingList.nearDate(test7));
+        LocalDateTime test = LocalDateTime.of(2024, 5, 22, 18, 0);
+        LocalDateTime ans = LocalDateTime.of(2024, 6, 3, 18, 0);
+        assertEquals(ans, sendingList.nearDate(test));
     }
 
     @Test
     public void testNearDate8(){
-        LocalDateTime test8 = LocalDateTime.of(2024, 4, 17, 18, 0);
-        LocalDateTime ans8 = LocalDateTime.of(2024, 4, 22, 18, 0);
-        assertEquals(ans8, sendingList.nearDate(test8));
+        LocalDateTime test = LocalDateTime.of(2024, 4, 17, 18, 0);
+        LocalDateTime ans = LocalDateTime.of(2024, 4, 22, 18, 0);
+        assertEquals(ans, sendingList.nearDate(test));
     }
 }
